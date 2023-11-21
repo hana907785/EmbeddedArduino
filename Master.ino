3fff075f561919bf396656ac851fc90f1df4efbd
sjoon0123 <150670544+sjoon0123@users.noreply.github.com>
2023-11-21 19:37:00 +0900

    Add files via upload

--- Master.ino
+++ Master.ino
@@ -6,15 +6,13 @@ const int S2 = 9;
 const int ledPins[] = {2, 3, 4, 5, 6};
 
 //정류장 인덱스
-int spaces[] = {11, 12, 13, 21, 22};
-
-
+int spaces[] = {11, 12, 21, 22, 31};
+int arrive = 0;
 byte s;
 //현재 버스 인덱스
 int currentBusIndex = 0;
-boolean arrive;
 int currentStop = spaces[currentBusIndex];
-
+int stop;
 void setup() {
   mySerial.begin(9600);
     SPI.begin();
@@ -35,8 +33,6 @@ byte transferAndWait(const byte what) {
   return a;
 }
 
-int old = 99; // 걍 쓰레기값
-
 void loop() {
   digitalWrite(S1, LOW);
 
@@ -45,42 +41,27 @@ void loop() {
   s = transferAndWait(0);
   digitalWrite(S1, HIGH);
   Serial.println(s);
-  Arrive();
-
-  /* 버튼에서 값을 연속으로 보내서 서진이한테도 무한으로 보내는 문제 발생
-   *  이 문제를 해결하기 위해 전에 받았던 값을 저장하고 다음에 받은 값을 비교하여 무한으로 신호를 보내는 문제를 해걀하려고 함
-   *  통신 spi로 바꿔도 될 것 같음 근데 아직 거기까진 생각을 안 함
-   */
   
-  if (old != arrive ) {
-    mySerial.write(arrive);
-  }
-  
-  old = arrive;
   
   if (Departure()) {
     // 다음으로 이동
     moveStop();
-
+    Serial.println(currentBusIndex);
     //현재 LED다 끔
     turnOffAllLEDs();
 
    //현재 정류장 LED킴
     digitalWrite(ledPins[currentBusIndex], HIGH);
   }
-  
-  digitalWrite(S1, LOW);
-  SPI.transfer(currentBusIndex);
-  digitalWrite(S1, HIGH);
-
+  Arrive();
+  mySerial.write(arrive);
   delay(1000);
 }
-
 void Arrive() {
-  if (currentBusIndex - s == 1) {
+  if (s - currentBusIndex == 1) {
     arrive = 1;
   }
-  else {
+  else if(s - currentBusIndex != 1) {
     arrive = 0;
   }
 }
